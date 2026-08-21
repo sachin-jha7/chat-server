0e49ee3af64b266e0499184915a3477fc6c9c70c
Sachin Jha <sachinjhagc@gmail.com>
2026-08-21 19:05:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chat-serve.netlify.app",
+        origin: "https://chat-server-five-jet.vercel.app",
         credentials: true
     }
 });
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 app.use(cors({
-    origin: "https://chat-serve.netlify.app",
+    origin: "https://chat-server-five-jet.vercel.app",
     credentials: true
 }));
 app.use(express.json());
